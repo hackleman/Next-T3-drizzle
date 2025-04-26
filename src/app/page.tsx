75248d18361530565c86b97fa0b6e63cc62a1b84
hackleman <hackleman.jason@gmail.com>
2025-04-26 14:44:23 -0700

    Rename DB and update schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,33 +2,19 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://4b5qkpip75.ufs.sh/f/nzpio9vOaLPE3Pq4qs6Ap6i8PEJOTFd2zUYhIaleQXu9gNvt",
-  "https://4b5qkpip75.ufs.sh/f/nzpio9vOaLPEnB8bXVvOaLPEwt9zfKih8gxuMV3kNqjydps6"
-];
-
-const mockImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url
-}));
-
 export default async function HomePage() {
 
-  const posts = await db.query.posts.findMany();
-
-  console.log(posts);
+  const images = await db.query.images.findMany({
+    orderBy: (model, { asc }) => asc(model.id)
+  });
 
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {
-          posts.map((post) => (
-            <div key={post.id}>{post.name}</div>
-          ))
-        }
-        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
-          <div key={index} className="w-48">
+        {images.map((image, index) => (
+          <div key={index} className="w-48 flex flex-col">
             <img src={image.url} alt="image" />
+            <div>{image.name}</div>
           </div>
         ))}
       </div>

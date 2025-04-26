6d9b15457c52ae061335cfc0884b7b2b9d70c7e7
hackleman <hackleman.jason@gmail.com>
2025-04-26 13:58:03 -0700

    scaffold root layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-import Link from "next/link";
+const mockUrls = [
+  "https://4b5qkpip75.ufs.sh/f/nzpio9vOaLPE3Pq4qs6Ap6i8PEJOTFd2zUYhIaleQXu9gNvt",
+  "https://4b5qkpip75.ufs.sh/f/nzpio9vOaLPEnB8bXVvOaLPEwt9zfKih8gxuMV3kNqjydps6"
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url
+}));
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello (gallery in progress)
+    <main className="">
+      <div className="flex flex-wrap gap-4">{
+        [...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} alt="image" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
